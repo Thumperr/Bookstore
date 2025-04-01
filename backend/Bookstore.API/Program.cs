b1aa2a49f429de0ca5ae019612cf5ac6f92a66b7
Thumperr <asdf;lkj@>
2025-04-01 15:56:09 -0600

    allowanyorigin with CORS

--- backend/Bookstore.API/Program.cs
+++ backend/Bookstore.API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options => {
     options.AddPolicy("AllowFrontend", 
         policy =>
         {
-            policy.WithOrigins("http://localhost:3001")
+            policy.AllowAnyOrigin()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
